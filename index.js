8c46e05b6b4d3bed8c77235610f3d442eba6db46
unknown <dev.thomas.soft+git@gmail.com>
2022-11-03 16:54:51 +0100

    Ajout d'animations

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const txtAnim = document.getElementById("myDescription");
 
+const presentation_contain_left = document.getElementById("presentation_contain_left");
+const presentation_contain_center = document.getElementById("presentation_contain_center");
+const presentation_contain_right = document.getElementById("presentation_contain_right");
+const progress_bar = document.getElementById("progress_bar");
+
 let lastScrollY = 0;
 
 new Typewriter(txtAnim,{
@@ -43,5 +48,67 @@ window.addEventListener("click", (e) =>
         hamburger.classList.toggle("open");
         links_container.classList.toggle("open");
         main_page.classList.toggle("open");
+    }
+});
+
+window.addEventListener("scroll", () =>
+{
+    let percentScroll = window.scrollY*100/(document.body.offsetHeight-window.innerHeight);
+    console.log(percentScroll);
+    if (percentScroll > 13)
+    {
+        presentation_contain_left.style.transform = "translateX(0)";
+        presentation_contain_left.style.opacity = 1;
+        presentation_contain_right.style.transform = "translateX(0)";
+        presentation_contain_right.style.opacity = 1;
+    }
+    else
+    {
+        presentation_contain_left.style.transform = "translateX(-100px)";
+        presentation_contain_left.style.opacity = 0;
+        presentation_contain_right.style.transform = "translateX(100px)";
+        presentation_contain_right.style.opacity = 0;
+    }
+    if (percentScroll > 20)
+    {
+        presentation_contain_center.style.transform = "translateY(0)";
+        presentation_contain_center.style.opacity = 1;
+    }
+    else
+    {
+        presentation_contain_center.style.transform = "translateY(100px)";
+        presentation_contain_center.style.opacity = 0;
+    }
+
+    if (percentScroll > 38)
+    {
+        competences.style.transform = "translateY(0)";
+        competences.style.opacity = 1;
+    }
+    else
+    {
+        competences.style.transform = "translateY(100px)";
+        competences.style.opacity = 0;
+    }
+
+    if (percentScroll > 40)
+    {
+        front_end_progress.style.width = 90+'%';
+        back_end_progress.style.width = 10+'%';
+        python_progress.style.width = 70+'%';
+        c_progress.style.width = 60+'%';
+        js_progress.style.width = 40+'%';
+        php_progress.style.width = 10+'%';
+    }
+
+    if (percentScroll > 65)
+    {
+        contact.style.transform = "translateY(0)";
+        contact.style.opacity = 1;
+    }
+    else
+    {
+        contact.style.transform = "translateY(100px)";
+        contact.style.opacity = 0;
     }
 });
